f070df6e83d2d377d0717f59d6abee33404326e9
SimonBouchier <simonbouchier1@gmail.com>
2017-06-15 16:33:26 +0100

    Added repo

---
+++ ProjectSnorlaxBackend/src/main/java/ProjectSnorlaxBackend/ProjectSnorlaxBackend/Entities/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package ProjectSnorlaxBackend.ProjectSnorlaxBackend.Entities;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuItemRepository extends JpaRepository<MenuItemEntity, Integer>{
+	
+}
